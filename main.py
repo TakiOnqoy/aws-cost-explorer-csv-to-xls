deb9f40af2684a8822597b9dfeb845bad602ceb9
André Elias <aeliasfoliveira@gmail.com>
2023-09-20 19:12:47 -0300

    using pandas to write to xls file

--- main.py
+++ main.py
@@ -1,41 +1,22 @@
 import csv
+import pandas as pd
 
 # Open the CSV file downloaded from AWS Cost Explorer
 with open('costs.csv', mode='r') as csv_file:
     csv_reader = csv.reader(csv_file)
-
-    # Skip the header row if it exists
-    #next(csv_reader, None)
-
     service = next(csv_reader)
     cost = next(csv_reader)
 
-service_output_file_path = 'zservice_list.csv'
-cost_output_file_path = 'zcost_list.csv'
-
+# Remove '($)' from 'Service' list items
 cleaned_service = [item.replace('($)', '') for item in service]
 
-with open(service_output_file_path, mode='w', newline='') as service_csv:
-    writer = csv.writer(service_csv)
+# Create a pandas DataFrame with 'Service' and 'Cost' columns
+df = pd.DataFrame({'Service': cleaned_service, 'Cost': cost})
 
-    # Write the header row
-    writer.writerow(['Service'])
+# Define the output Excel file path
+excel_output_file_path = 'structured_costs.xlsx'
 
-    # Write each 'Service' item as a separate row
-    for service_item in cleaned_service:
-        writer.writerow([service_item])
+# Write the DataFrame to an Excel file with 'Service' and 'Cost' columns
+df.to_excel(excel_output_file_path, index=False)
 
-print(f"'Service' list has been written to '{service_output_file_path}'.")
-
-# Write the 'Cost' list to a CSV file
-with open(cost_output_file_path, mode='w', newline='') as cost_csv:
-    writer = csv.writer(cost_csv)
-
-    # Write the header row
-    writer.writerow(['Cost'])
-
-    # Write each 'Cost' item as a separate row
-    for cost_item in cost:
-        writer.writerow([cost_item])
-
-    print(f"'Cost' list has been written to '{cost_output_file_path}'.")
+print(f"Data has been written to '{excel_output_file_path}'.")
